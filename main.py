3058b2b51782ea523944c25e0172f1f53f9d1145
Даниил <40400854+thedeaddan@users.noreply.github.com>
2022-05-24 11:37:32 +0300

    Сделан переход во второе окно

--- main.py
+++ main.py
@@ -2,16 +2,27 @@ from PyQt5 import QtWidgets
 from gui import first_window,second_window,third_window
 import sys
 
-
-class mywindow(QtWidgets.QMainWindow):
+Open = False
+class Second(QtWidgets.QMainWindow):
     def __init__(self):
-        super(mywindow, self).__init__()
+        super(Second, self).__init__()
+        self.ui = second_window.Ui_MainWindow()
+        self.ui.setupUi(self)
+
+class First(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(First, self).__init__()
         self.ui = first_window.Ui_MainWindow()
         self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.open_second)
+
+    def open_second(self):
+        w1.setVisible(False)
+        self.w2 = Second()
+        self.w2.show()
 
 
 app = QtWidgets.QApplication([])
-application = mywindow()
-application.show()
-
+w1 = First()
+w1.show()
 sys.exit(app.exec())
