ecca18a776089f30ca9e2382fa5ffd60a882eab7
Даниил <40400854+thedeaddan@users.noreply.github.com>
2022-05-24 11:21:41 +0300

    Изменил архитектуру библиотек

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from PyQt5 import QtWidgets
-from first_window import Ui_MainWindow
+from gui import first_window,second_window,third_window
 import sys
 
 
 class mywindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(mywindow, self).__init__()
-        self.ui = Ui_MainWindow()
+        self.ui = first_window.Ui_MainWindow()
         self.ui.setupUi(self)
 
 
